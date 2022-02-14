720f7dbd2da9b2a68f05025706a0d695f0755d5a
Lee Wei Min <lee.wei.min@u.nus.edu>
2022-02-14 11:59:18 +0800

    Implement sorting

--- SimpleDBEngine/src/simpledb/materialize/RecordComparator.java
+++ SimpleDBEngine/src/simpledb/materialize/RecordComparator.java
@@ -6,20 +6,29 @@ import simpledb.query.*;
 
 /**
  * A comparator for scans.
+ * 
  * @author Edward Sciore
  */
 public class RecordComparator implements Comparator<Scan> {
    private List<String> fields;
-   
+   private List<List<String>> sortFields;
+
    /**
     * Create a comparator using the specified fields,
     * using the ordering implied by its iterator.
+    * 
     * @param fields a list of field names
     */
+
    public RecordComparator(List<String> fields) {
       this.fields = fields;
    }
-   
+
+   // Adding extra param to avoid type erasure error (with just using sortFields)
+   public RecordComparator(List<List<String>> sortFields, boolean isSortField) {
+      this.sortFields = sortFields;
+   }
+
    /**
     * Compare the current records of the two specified scans.
     * The sort fields are considered in turn.
@@ -28,18 +37,39 @@ public class RecordComparator implements Comparator<Scan> {
     * of the comparison.
     * If the two records have the same values for all
     * sort fields, then the method returns 0.
+    * 
     * @param s1 the first scan
     * @param s2 the second scan
-    * @return the result of comparing each scan's current record according to the field list
+    * @return the result of comparing each scan's current record according to the
+    *         field list
     */
    public int compare(Scan s1, Scan s2) {
-      for (String fldname : fields) {
-         Constant val1 = s1.getVal(fldname);
-         Constant val2 = s2.getVal(fldname);
-         int result = val1.compareTo(val2);
-         if (result != 0)
-            return result;
+      if (fields != null) {
+         for (String fldname : fields) {
+            Constant val1 = s1.getVal(fldname);
+            Constant val2 = s2.getVal(fldname);
+            int result = val1.compareTo(val2);
+            if (result != 0)
+               return result;
+         }
+         return 0;
+      } else {
+         for (List<String> sortField : sortFields) {
+            String fldname = sortField.get(0);
+            String order = sortField.get(1);
+
+            Constant val1 = s1.getVal(fldname);
+            Constant val2 = s2.getVal(fldname);
+
+            int result = val1.compareTo(val2);
+
+            if (order.equals("desc")) {
+               result *= -1;
+            }
+            if (result != 0)
+               return result;
+         }
+         return 0;
       }
-      return 0;
    }
 }
--- SimpleDBEngine/src/simpledb/parse/QueryData.java
+++ SimpleDBEngine/src/simpledb/parse/QueryData.java
@@ -6,13 +6,15 @@ import simpledb.query.*;
 
 /**
  * Data for the SQL <i>select</i> statement.
+ * 
  * @author Edward Sciore
  */
 public class QueryData {
    private List<String> fields;
    private Collection<String> tables;
    private Predicate pred;
-   
+   private List<List<String>> sortFields;
+
    /**
     * Saves the field and table list and predicate.
     */
@@ -21,41 +23,60 @@ public class QueryData {
       this.tables = tables;
       this.pred = pred;
    }
-   
+
+   public QueryData(List<String> fields, Collection<String> tables, Predicate pred, List<List<String>> sortFields) {
+      this.fields = fields;
+      this.tables = tables;
+      this.pred = pred;
+      this.sortFields = sortFields;
+   }
+
    /**
     * Returns the fields mentioned in the select clause.
+    * 
     * @return a list of field names
     */
    public List<String> fields() {
       return fields;
    }
-   
+
+   /**
+    * Returns the sort fields mentioned in the select clause.
+    * 
+    * @return a list of sort field names
+    */
+   public List<List<String>> sortFields() {
+      return sortFields;
+   }
+
    /**
     * Returns the tables mentioned in the from clause.
+    * 
     * @return a collection of table names
     */
    public Collection<String> tables() {
       return tables;
    }
-   
+
    /**
     * Returns the predicate that describes which
     * records should be in the output table.
+    * 
     * @return the query predicate
     */
    public Predicate pred() {
       return pred;
    }
-   
+
    public String toString() {
       String result = "select ";
       for (String fldname : fields)
          result += fldname + ", ";
-      result = result.substring(0, result.length()-2); //remove final comma
+      result = result.substring(0, result.length() - 2); // remove final comma
       result += " from ";
       for (String tblname : tables)
          result += tblname + ", ";
-      result = result.substring(0, result.length()-2); //remove final comma
+      result = result.substring(0, result.length() - 2); // remove final comma
       String predstring = pred.toString();
       if (!predstring.equals(""))
          result += " where " + predstring;
