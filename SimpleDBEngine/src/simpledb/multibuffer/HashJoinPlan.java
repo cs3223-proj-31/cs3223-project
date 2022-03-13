1f48648e5d3930f4ad9a97ba0269687f5f5746b0
Riyadh H. Alzahrani <69278589+riyadh-h@users.noreply.github.com>
2022-03-13 23:21:07 +0800

    Modify HashJoinPlan attributes and constructor

--- SimpleDBEngine/src/simpledb/multibuffer/HashJoinPlan.java
+++ SimpleDBEngine/src/simpledb/multibuffer/HashJoinPlan.java
@@ -1,24 +1,37 @@
 package simpledb.multibuffer;
 
 import simpledb.tx.Transaction;
+import simpledb.materialize.TempTable;
 import simpledb.plan.Plan;
+import simpledb.plan.SelectPlan;
+import simpledb.plan.TablePlan;
 import simpledb.query.Scan;
+import simpledb.query.UpdateScan;
+import simpledb.query.Constant;
 import simpledb.query.Predicate;
 import simpledb.record.Schema;
+import simpledb.record.TableScan;
 
+/**
+ * This class follows the definition of hash joins as mentioned in
+ * Section 14.7 of Sciore (2020).
+ * @author riyadh-h
+ *
+ */
 public class HashJoinPlan implements Plan {
 	private Transaction tx;
-	private Plan lhs, rhs;
+	private Plan p1, p2;  // p1 corresponds to T1; similar for p2 and T2.
 	private Schema schema = new Schema();
 	private Predicate pred;
+	private String joinfield;
 	
-	public HashJoinPlan(Transaction tx, Plan lhs, Plan rhs, Predicate pred) {
+	public HashJoinPlan(Transaction tx, Plan p1, Plan p2, Predicate pred) {
 		this.tx = tx;
-		this.lhs = lhs;
-		this.rhs = rhs;
+		this.p1 = p1;
+		this.p2 = p2;
 		this.pred = pred;
-		schema.addAll(lhs.schema());
-		schema.addAll(rhs.schema());
+		schema.addAll(p1.schema());
+		schema.addAll(p2.schema());
 	}
 
 	@Override
