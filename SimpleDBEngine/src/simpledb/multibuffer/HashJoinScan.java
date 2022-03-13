d7f9854762685018c3ddac625d4b1adcedfb6186
Riyadh H. Alzahrani <69278589+riyadh-h@users.noreply.github.com>
2022-03-13 23:52:31 +0800

    Implement methods of HashJoinScan

--- SimpleDBEngine/src/simpledb/multibuffer/HashJoinScan.java
+++ SimpleDBEngine/src/simpledb/multibuffer/HashJoinScan.java
@@ -4,47 +4,65 @@ import simpledb.query.Constant;
 import simpledb.query.Scan;
 
 public class HashJoinScan implements Scan {
+	private Scan[] subscans;
+	private Scan currentscan;
+	private int currentscanidx;
+	
+	public HashJoinScan(Scan[] subscans) {
+		this.subscans = subscans;
+		beforeFirst();
+	}
 
 	@Override
 	public void beforeFirst() {
 		// TODO Auto-generated method stub
-		
+		currentscanidx = 0;
+		currentscan = subscans[currentscanidx];
+		currentscan.beforeFirst();
 	}
 
 	@Override
 	public boolean next() {
-		// TODO Auto-generated method stub
-		return false;
+		while (!currentscan.next()) {
+			currentscan.close();
+			currentscanidx++;
+			if (currentscanidx >= subscans.length)
+				return false;
+			currentscan = subscans[currentscanidx];
+			currentscan.beforeFirst();
+		}
+		return true;
 	}
 
 	@Override
 	public int getInt(String fldname) {
 		// TODO Auto-generated method stub
-		return 0;
+		return currentscan.getInt(fldname);
 	}
 
 	@Override
 	public String getString(String fldname) {
 		// TODO Auto-generated method stub
-		return null;
+		return currentscan.getString(fldname);
 	}
 
 	@Override
 	public Constant getVal(String fldname) {
 		// TODO Auto-generated method stub
-		return null;
+		return currentscan.getVal(fldname);
 	}
 
 	@Override
 	public boolean hasField(String fldname) {
 		// TODO Auto-generated method stub
-		return false;
+		return currentscan.hasField(fldname);
 	}
 
 	@Override
 	public void close() {
 		// TODO Auto-generated method stub
-		
+		for (int i = currentscanidx; i < subscans.length; i++)
+			subscans[i].close();
 	}
 
 }
