4b6317d20fb58160ca3feda0c6acf0a29720ae5b
Riyadh H. Alzahrani <69278589+riyadh-h@users.noreply.github.com>
2022-03-13 21:18:18 +0800

    Add HashJoinScan.java

---
+++ SimpleDBEngine/src/simpledb/multibuffer/HashJoinScan.java
@@ -0,0 +1,50 @@
+package simpledb.multibuffer;
+
+import simpledb.query.Constant;
+import simpledb.query.Scan;
+
+public class HashJoinScan implements Scan {
+
+	@Override
+	public void beforeFirst() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean next() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getInt(String fldname) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getString(String fldname) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Constant getVal(String fldname) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasField(String fldname) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
