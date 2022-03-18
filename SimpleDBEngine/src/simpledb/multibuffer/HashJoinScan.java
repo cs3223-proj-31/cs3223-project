63a59d194e47061852e798ef8ab64625279c9106
Riyadh H. Alzahrani <69278589+riyadh-h@users.noreply.github.com>
2022-03-18 15:53:34 +0800

    Use TempTables instead of Scans

--- SimpleDBEngine/src/simpledb/multibuffer/HashJoinScan.java
+++ SimpleDBEngine/src/simpledb/multibuffer/HashJoinScan.java
@@ -2,6 +2,7 @@ package simpledb.multibuffer;
 
 import java.util.HashMap;
 import simpledb.materialize.TempTable;
+import simpledb.plan.Plan;
 import simpledb.query.Constant;
 import simpledb.query.Scan;
 import simpledb.query.UpdateScan;
@@ -9,7 +10,7 @@ import simpledb.record.Schema;
 import simpledb.tx.Transaction;
 
 public class HashJoinScan implements Scan {
-	private Scan[] lhsscans, rhsscans;
+	private TempTable[] lhstts, rhstts;
 	private String lhsjoinfld, rhsjoinfld;
 	private Transaction tx;
 	private Schema lhssch;
@@ -18,57 +19,74 @@ public class HashJoinScan implements Scan {
 	private Scan lhscurscan, rhscurscan, tempscan;
 	private HashMap<Constant, UpdateScan> ht;
 	
-	public HashJoinScan(Scan[] lhsscans, Scan[] rhsscans, String lhsjoinfld, String rhsjoinfld, Transaction tx, Schema lhssch) {
-		this.lhsscans = lhsscans;
-		this.rhsscans = rhsscans;
+	public HashJoinScan(TempTable[] lhstts, TempTable[] rhstts, String lhsjoinfld, String rhsjoinfld, Transaction tx, Schema lhssch) {
+		this.lhstts = lhstts;
+		this.rhstts = rhstts;
 		this.lhsjoinfld = lhsjoinfld;
 		this.rhsjoinfld = rhsjoinfld;
 		this.tx = tx;
 		this.lhssch = lhssch;
+		ht = new HashMap<>();
 		beforeFirst();
 	}
 
 	@Override
 	public void beforeFirst() {
+		if (lhscurscan != null) {
+			lhscurscan.close();
+		}
+		
+		if (rhscurscan != null) {
+			rhscurscan.close();
+		}
+		
 		scanpairidx = 0;
-		lhscurscan = lhsscans[scanpairidx];
-		rhscurscan = rhsscans[scanpairidx];
+		lhscurscan = lhstts[scanpairidx].open();
+		rhscurscan = rhstts[scanpairidx].open();
 		buildHashTableOnLhsPartScan();
 	}
 	
 	public void buildHashTableOnLhsPartScan() {
+		for (Constant key : ht.keySet()) {
+			ht.get(key).close();
+		}
+		
 		ht.clear();
 		lhscurscan.beforeFirst();
 		while (lhscurscan.next()) {
-			Constant joinfieldval = lhscurscan.getVal(lhsjoinfld);
+			Constant joinfieldval = lhscurscan.getVal(lhsjoinfld);			
 			ht.putIfAbsent(joinfieldval, new TempTable(tx, lhssch).open());
 			UpdateScan mapscan = ht.get(joinfieldval);
 			
 			// Copy record of lhscurscan to tempscan.
 			for (String fldname : lhssch.fields()) {
+				mapscan.insert();
 				mapscan.setVal(fldname, lhscurscan.getVal(fldname));
 			}
-			mapscan.insert();
 		}
 		
 		// Reset pointers to point at the first record.
 		for (Constant key : ht.keySet()) {
 			ht.get(key).beforeFirst();
 		}
+		
+		lhscurscan.close();
 	}
 	
 	@Override
 	public boolean next() {
-		while (tempscan == null || !tempscan.next()) {
+		while (tempscan == null || !tempscan.next()) {			
 			while (!rhscurscan.next()) {
+				rhscurscan.close();
+				
 				scanpairidx++;
 				// Assume lhsscans.length == rhsscans.length
-				if (scanpairidx >= lhsscans.length) {
+				if (scanpairidx >= lhstts.length) {
 					return false;
 				}
 				
-				lhscurscan = lhsscans[scanpairidx];
-				rhscurscan = rhsscans[scanpairidx];
+				lhscurscan = lhstts[scanpairidx].open();
+				rhscurscan = rhstts[scanpairidx].open();
 				buildHashTableOnLhsPartScan();
 				
 				rhscurscan.beforeFirst();
@@ -122,13 +140,7 @@ public class HashJoinScan implements Scan {
 			ht.get(key).close();
 		}
 		
-		ht.clear();
-		
-		// Should cover lhscurscan and rhscurscan.
-		for (int i = 0; i < lhsscans.length; i++) {
-			lhsscans[i].close();
-			rhsscans[i].close();
-		}
+		ht.clear();		
 	}
 
 }
